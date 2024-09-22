ad503fb8149e36db9ae144a06f3d29932fa5e485
kcarww <carlos.oe.cc@gmail.com>
2024-09-22 16:40:49 -0300

    feat: working with express validator

--- noticias/app/routes/admin.js
+++ noticias/app/routes/admin.js
@@ -1,13 +1,25 @@
+const { check, validationResult } = require('express-validator');
+
 module.exports = function(app) {
     app.get('/formulario_inclusao_noticia', function(req, res) {
         res.render("admin/form_add_noticia");
     });
 
-    app.post('/noticias/salvar', function(req, res) {
+    app.post('/noticias/salvar',[
+        check('titulo').not().isEmpty().withMessage('Título é obrigatório')
+    ], function(req, res) {
         const noticia = req.body;
 
+        const errors = validationResult(req)
+        if (!errors.isEmpty()) {
+            return res.status(400).render('admin/form_add_noticia', {
+                errors: errors.array(),
+                noticia: noticia  
+            })
+        }
+
         const connection = app.config.dbConnection()
-        const noticiasModel = new app.app.models.noticiasModel
+        const noticiasModel = app.app.models.noticiasModel
 
         noticiasModel.salvarNoticia(connection, noticia, function(error, result) {
             res.redirect('/noticias')
--- noticias/config/server.js
+++ noticias/config/server.js
@@ -1,11 +1,14 @@
 const express = require('express')
 const consign = require('consign')
 const bodyParser = require('body-parser')
+
 const app = express(); 
 
 app.set('view engine', 'ejs')
 app.set('views', './app/views')
+
 app.use(bodyParser.urlencoded({extended: true}))
+
 
 
 consign()
